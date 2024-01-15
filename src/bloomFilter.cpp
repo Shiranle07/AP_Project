2da51e0e42f6cddb110b9d43036535a0dfc2b0a7
Shiran Levi <shiranle07@gmail.com>
2024-01-15 15:15:34 +0200

    bloom filter

---
+++ src/bloomFilter.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+#include <string>
+
+//hash<string> hash1;
+//hash<hash<string>> hash2;
+
+int main() {
+    std::cout << "abc" << std::endl;
+     return 0;
+}
