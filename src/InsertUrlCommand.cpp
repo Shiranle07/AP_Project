7a4b5d2048606e03661c759a56ef03ebeafae3b6
Shiran Levi <shiranle07@gmail.com>
2024-01-19 11:33:25 +0200

    add ;

--- src/InsertUrlCommand.cpp
+++ src/InsertUrlCommand.cpp
@@ -7,11 +7,11 @@ using namespace std;
 
 // constructor implement
 InsertURLCommand::InsertURLCommand(string url, vector<string>& urlList, BloomFilter& insertBloom)
-            : url(url), urlList(urlList), insertBloom(insertBloom) {}
+            : url(url), urlList(urlList), insertBloom(insertBloom) {};
 
 
 // add the url to the url vector and add the url to the bloom filter arrary according to the hash result
 void InsertURLCommand::execute(){
     this->urlList.push_back(this->url);
     this->insertBloom.add(this->url);
-}
+};
