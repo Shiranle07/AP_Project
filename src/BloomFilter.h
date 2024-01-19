80f5484ff151492c19aef162e139035ecb289173
Shiran Levi <shiranle07@gmail.com>
2024-01-19 11:46:55 +0200

    bloomFilter h file

---
+++ src/BloomFilter.h
@@ -0,0 +1,29 @@
+#ifndef BLOOMFILTER_H
+#define BLOOMFILTER_H
+
+#include <iostream>
+using namespace std;
+#include <string>
+#include <map>
+#include "IHash.h"
+
+// the bloom filter class
+class BloomFilter{
+    private:
+        int arrSize;
+        int* bloomArr;
+        map<int, bool> hashToRunMap;
+        map<int, IHash> hashesMap;
+
+//constructor
+    public:
+        BloomFilter(int arrSize, map<int, bool> hashToRunMap, map<int, IHash> hashesMap);
+
+// adds URL to the bloom filter array
+    void add(string url);
+
+// checks if an URL exists in the bloom filter array
+    bool check();
+};
+
+#endif
