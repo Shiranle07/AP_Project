3342b1ffd58bc6d3f7d2984e83c1dd4bfa9d55e1
Your Name <you@example.com>
2024-01-22 19:06:14 +0200

    change all the strings to vectors

--- tests/first_line_format.cpp
+++ tests/first_line_format.cpp
@@ -16,56 +16,56 @@ TEST(InputValidationTest, CheckFirstLine) {
     InputValidation inputValidation(commands, hashToRunMap, bloomFilter);
 
     // Test with a normal array size
-    EXPECT_TRUE(inputValidation.checkFirstLine("256 1 2"));
+    EXPECT_TRUE(inputValidation.checkFirstLine({256, 1, 2}));
 
     // Test with a negative array size
-    EXPECT_FALSE(inputValidation.checkFirstLine("-8 2 1"));
+    EXPECT_FALSE(inputValidation.checkFirstLine({-8, 2, 2}));
     
     // Test that array size = 0
-    EXPECT_FALSE(inputValidation.checkFirstLine("0 2 1"));
+    EXPECT_FALSE(inputValidation.checkFirstLine({0, 2, 1}));
 
     // Test with a string containing also letter in array size
-    EXPECT_FALSE(inputValidation.checkFirstLine("128a 1 2"));
+    EXPECT_FALSE(inputValidation.checkFirstLine({128a, 1, 2}));
 
     // Test with a string containing no numbers in array size
-    EXPECT_FALSE(inputValidation.checkFirstLine("a 1 1"));
+    EXPECT_FALSE(inputValidation.checkFirstLine({a, 1, 1}));
 
     // Test with an empty string
-    EXPECT_FALSE(inputValidation.checkFirstLine(""));
+    EXPECT_FALSE(inputValidation.checkFirstLine({}));
 }
 
 // Checks that the hash number is 1 or 2 only.
 TEST(InputValidationTest, CheckFirstLine) {
     // Test with a normal hash numbers
-    EXPECT_TRUE(inputValidation.checkFirstLine("8 1 2"));
+    EXPECT_TRUE(inputValidation.checkFirstLine({8, 1, 2}));
 
     // Test with a big hash number
-    EXPECT_FALSE(inputValidation.checkFirstLine("8 3 2"));
+    EXPECT_FALSE(inputValidation.checkFirstLine({8, 3, 2}));
 
     // Test with a small hash number
-    EXPECT_FALSE(inputValidation.checkFirstLine("8 3 0"));
+    EXPECT_FALSE(inputValidation.checkFirstLine({8, 3, 0}));
 
     // Test with a negative hash number
-    EXPECT_FALSE(inputValidation.checkFirstLine("8 3 -2"));
+    EXPECT_FALSE(inputValidation.checkFirstLine({8, 3, -2}));
 
     // Test with a string containing also letter in hash number
-    EXPECT_FALSE(inputValidation.checkFirstLine("8 1 6"));
+    EXPECT_FALSE(inputValidation.checkFirstLine({8, 1, 6}));
 
     // Test with no hash number
-    EXPECT_FALSE(inputValidation.checkFirstLine("8"));
+    EXPECT_FALSE(inputValidation.checkFirstLine({8}));
 }
 
 // Checks that the first input has more than one digit.
 TEST(InputValidationTest, CheckFirstLine) {
     // Test with a string containing more than one number
-    EXPECT_TRUE(inputValidation.checkFirstLine("8 1 2"));
+    EXPECT_TRUE(inputValidation.checkFirstLine({8, 1, 2}));
 
     // Test with a string containing only one number
-    EXPECT_FALSE(inputValidation.checkFirstLine("256"));
+    EXPECT_FALSE(inputValidation.checkFirstLine({256}));
 
     // Test with a string containing no numbers
-    EXPECT_FALSE(inputValidation.checkFirstLine("a8b2c1"));
+    EXPECT_FALSE(inputValidation.checkFirstLine({a8b2c1}));
 
     // Test with an empty string
-    EXPECT_FALSE(inputValidation.checkFirstLine(""));
+    EXPECT_FALSE(inputValidation.checkFirstLine({}));
 }
