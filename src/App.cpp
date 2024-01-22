572784eafdb4f3a07ec27780de91942cfe2f8fee
Shiran Levi <shiranle07@gmail.com>
2024-01-22 13:59:42 +0200

    add main

--- src/App.cpp
+++ src/App.cpp
@@ -52,29 +52,3 @@ void App::run() {
     delete checker; 
 }
 
-
-int main(){
-    IHash* hash1 = new HashOne();
-    IHash* hash2 = new HashTwo();
-    map<int, IHash*> hashesMap;
-    hashesMap[1] = hash1;
-    hashesMap[2] = hash2;
-    ICommand* check = new CheckUrlCommand();
-    ICommand* insert = new InsertUrlCommand();
-    map<int, ICommand*> commands;
-    commands[1] = insert;
-    commands[2] = check;
-    map<int, bool> hashToRunMap;
-
-    BloomFilter* bloomFilter = new BloomFilter(hashToRunMap, hashesMap);
-    App app(commands, hashesMap, hashToRunMap, bloomFilter);
-    app.run();
-
-    delete hash1;
-    delete hash2;
-    delete check;
-    delete insert;
-    delete bloomFilter;
-
-}
-
