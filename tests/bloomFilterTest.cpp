bcd25f0301449e87cf722d313b145b8425ef653a
Your Name <you@example.com>
2024-01-24 16:35:28 +0200

    last bloom edit

--- tests/bloomFilterTest.cpp
+++ tests/bloomFilterTest.cpp
@@ -12,23 +12,6 @@ using namespace std;
 #include "../src/InsertUrlCommand.cpp"
 
 
-// Test case for checking if the string starts with digit 1 or 2
-/**TEST(FormatTest, BloomFilterTest) {
-    
-    IHash* hash1 = new HashOne();
-    IHash* hash2 = new HashTwo();
-    map<int, IHash*> hashesMap;
-    hashesMap[1] = hash1;
-    hashesMap[2] = hash2;
-    map<int, bool> hashToRunMap;
-
-    vector<string> input_vector{"256", "1", "2"};
-    // Test with a normal array size
-    EXPECT_TRUE(inputValidation->checkFirstLine(input_vector));
-*/
-
-
-
 TEST(BloomFilterTest, AddAndCheckURL) {
     // Create Bloom filter with specific size
     IHash* hash1 = new HashOne();
